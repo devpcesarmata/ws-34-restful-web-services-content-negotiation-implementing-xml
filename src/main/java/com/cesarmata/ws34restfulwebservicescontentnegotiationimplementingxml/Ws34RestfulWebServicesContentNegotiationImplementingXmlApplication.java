9d70d0b4395a818b006eb9f34b531a500977102c
César <dev.pcesarmata@gmail.com>
2020-08-25 17:40:55 -0500

    init commit.

---
+++ src/main/java/com/cesarmata/ws34restfulwebservicescontentnegotiationimplementingxml/Ws34RestfulWebServicesContentNegotiationImplementingXmlApplication.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws34restfulwebservicescontentnegotiationimplementingxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws34RestfulWebServicesContentNegotiationImplementingXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws34RestfulWebServicesContentNegotiationImplementingXmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesarmata/ws34restfulwebservicescontentnegotiationimplementingxml/Ws34RestfulWebServicesContentNegotiationImplementingXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws34restfulwebservicescontentnegotiationimplementingxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws34RestfulWebServicesContentNegotiationImplementingXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
