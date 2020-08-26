503ac33b1b828d7450a938c06556544eb5b10c07
César <dev.pcesarmata@gmail.com>
2020-08-26 10:47:39 -0500

    Se concluye clase 34.

--- src/main/java/com/cesarmata/ws34restfulwebservicescontentnegotiationimplementingxml/Ws34RestfulWebServicesContentNegotiationImplementingXmlApplication.java
+++ src/main/java/com/cesarmata/ws34restfulwebservicescontentnegotiationimplementingxml/Ws34RestfulWebServicesContentNegotiationImplementingXmlApplication.java
@@ -1,7 +1,12 @@
 package com.cesarmata.ws34restfulwebservicescontentnegotiationimplementingxml;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class Ws34RestfulWebServicesContentNegotiationImplementingXmlApplication {
@@ -9,5 +14,11 @@ public class Ws34RestfulWebServicesContentNegotiationImplementingXmlApplication 
 	public static void main(String[] args) {
 		SpringApplication.run(Ws34RestfulWebServicesContentNegotiationImplementingXmlApplication.class, args);
 	}
-
+	
+	@Bean
+	public LocaleResolver localResolver() {
+		AcceptHeaderLocaleResolver localResolver = new AcceptHeaderLocaleResolver();
+		localResolver.setDefaultLocale(Locale.US);
+		return localResolver;
+	}
 }
